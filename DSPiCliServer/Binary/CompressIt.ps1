7805318f82995615ed1a7e7659db5fd16f2d2877
Mark Zachmann <MZachmann@users.noreply.github.com>
2026-05-09 09:15:18 -0400

    add compression batch

---
+++ DSPiCliServer/Binary/CompressIt.ps1
@@ -0,0 +1,4 @@
+Compress-Archive -Force -Path 'DSPiCliremote-Winx64' -DestinationPath 'DSPiCliremote-Winx64.zip'
+Compress-Archive -Force -Path 'DSPiCliremote-MacOSX' -DestinationPath 'DSPiCliremote-MacOSX.zip'
+Compress-Archive -Force -Path 'DSPiCliremote-LinuxX64' -DestinationPath 'DSPiCliremote-LinuxX64.zip'
+Compress-Archive -Force -Path 'DSPiCliremote-LinuxArm64' -DestinationPath 'DSPiCliremote-LinuxArm64.zip'
